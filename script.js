1c188dc3198a87cdce7c903a64d500d9c157a3a5
firat <firat.duzgun@hotmail.com>
2024-10-08 20:14:11 +0300

    finish light-hearted joke

---
+++ script.js
@@ -0,0 +1,17 @@
+const leftContent = document.querySelector('.left-content');
+
+const magicButton = document.getElementById('magic-button');
+
+magicButton.addEventListener('click', () => {
+  if (leftContent.style.backgroundImage === 'url("/image-bad.jpeg")') {
+    document.getElementsByClassName('left-content')[0].style.backgroundImage =
+      'url("/image-good.jpeg")';
+    document.getElementById('damla').src = 'damla-good.svg';
+    document.getElementById('damla-title').textContent = 'animeci zammuro';
+  } else {
+    document.getElementsByClassName('left-content')[0].style.backgroundImage =
+      'url("/image-bad.jpeg")';
+    document.getElementById('damla').src = 'damla-bad.svg';
+    document.getElementById('damla-title').textContent = 'hamuri zammuro';
+  }
+});
